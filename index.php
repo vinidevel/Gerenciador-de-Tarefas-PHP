8c6182ea72a6721219dab5d9a763238b48dd63df
Vinicius Cavalcanti <vini_ide@yahoo.com.br>
2022-07-01 22:49:31 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+
+
+<?php session_start(); ?>
+
+<html>
+    <head>
+        <title> Gerenciador de Tarefas </title>
+    </head>
+
+    <body>
+
+        <h1> Gerenciador de Tarefas </h1>
+
+        <form>
+    <fieldset>
+        <legend> Nova Tarefa </legend>
+            <label>
+                Tarefa:
+                <input type="text" name="nome" />
+            </label>
+            <input type="submit" value="Cadastrar" />
+    </fieldset>
+</form>   
+
+<?php
+
+    
+
+    if (isset($_GET['nome'])) {
+        $_SESSION['lista_tarefas'][] = $_GET['nome'];
+
+    }
+
+    $lista_tarefas = array();
+
+    if (isset($_SESSION['lista_tarefas'])) {
+        $lista_tarefas = $_SESSION['lista_tarefas'];
+    }
+
+?>
+
+<table>
+        <tr>
+            <th> Tarefas </th> 
+        </tr>  
+        
+        <?php foreach($lista_tarefas as $tarefa) : ?> 
+            <tr>
+                <td><?php echo $tarefa;?></td>
+            </tr>
+        <?php endforeach; ?>
+
+    </table> 
+
+      
+    </body>        
+</html>
+
+      
+
+
+
